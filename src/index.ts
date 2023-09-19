a7c12acbb08ec5c2af141663ba792babf4945dc2
vcoman4 <vladut.coman@tectu.io>
2023-09-19 15:00:35 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import { launch, } from 'puppeteer'
 import * as fs from 'fs'
 
-const html = fs.readFileSync('template.html', 'utf-8');
+const HTML_FILE_NAME = "test.html";
+const NEW_PDF_FILE_NAME = "template4.pdf";
+
+// First parameter is the name of the html file to be read
+const html = fs.readFileSync(HTML_FILE_NAME, "utf-8");
 
 async function exportWebsiteAsPdf(html: string, outputPath: string) {
   // Create a browser instance
   const browser = await launch({
-    headless: 'new'
+    headless: "new",
   });
   // Create a new page
   const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: 'domcontentloaded' });
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
   // To reflect CSS used for screens instead of print
-  await page.emulateMediaType('screen');
+  await page.emulateMediaType("screen");
   // Download the PDF
   const PDF = await page.pdf({
     path: outputPath,
-    margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+    margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
     printBackground: true,
-    format: 'A4',
+    format: "A4",
   });
   // Close the browser instance
   await browser.close();
@@ -26,11 +30,14 @@ async function exportWebsiteAsPdf(html: string, outputPath: string) {
   return PDF;
 }
 
-await exportWebsiteAsPdf(html, 'template_test_1.pdf').then(() => {
-  console.log('PDF created successfully.');
-}).catch((error) => {
-  console.error('Error creating PDF:', error);
-});
+// The second parameter is the name of the PDF file to be created
+await exportWebsiteAsPdf(html, NEW_PDF_FILE_NAME)
+  .then(() => {
+    console.log("PDF created successfully.");
+  })
+  .catch((error) => {
+    console.error("Error creating PDF:", error);
+  });
 
 
 
